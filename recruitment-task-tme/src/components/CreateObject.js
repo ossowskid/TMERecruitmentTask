34b630f8d89431ec8351284979eecd3bf810d53f
ossowskid <ossowski.dawid@gmail.com>
2021-08-29 14:38:10 +0200

    wip

--- recruitment-task-tme/src/components/CreateObject.js
+++ recruitment-task-tme/src/components/CreateObject.js
@@ -1,25 +1,76 @@
+import { useEffect, useState } from "react";
+
 export const CreateObject = () => {
   let alphabet = "abcdefghijklmnoprstuwxyz";
-  const generateRandomLetter = () => {
-    let randomPlateLetters =
-      alphabet[Math.floor(Math.random() * alphabet.length)];
-    return randomPlateLetters;
-  };
-  const generateRandomNum = () => {
-    let plateNumber = Math.floor(Math.random() * 9) + 1;
-    return plateNumber;
-  };
 
-  let obj = [];
-  for (let i = 1; i < 20000; i++) {
-    obj[i] = {
-      plates: `${generateRandomLetter()}${generateRandomLetter()}${generateRandomLetter()}${generateRandomNum()}${generateRandomNum()}${generateRandomNum()}`,
+  const [user, setUser] = useState([]);
+  const [names, setNames] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch(
+        "https://randomuser.me/api/?results=20&name&inc=name"
+      );
+      const data = await response.json();
+      setNames(data.results);
     };
-  }
+    getData();
+  }, []);
+  useEffect(() => {
+    if (names.length < 1) {
+      return;
+    }
+    const randomPhoneNumber = () => {
+      return Math.floor(Math.random() * 999999999) + 0;
+    };
+    const generateRandomSpeed = (min, max) => {
+      return Math.floor(Math.random() * (max - min)) + min;
+    };
+    const randomPlate = () => {
+      let randomPlateLetters =
+        alphabet[Math.floor(Math.random() * alphabet.length)] +
+        alphabet[Math.floor(Math.random() * alphabet.length)] +
+        alphabet[Math.floor(Math.random() * alphabet.length)];
+      let plateNumber = Math.floor(Math.random() * 999) + 0;
+      let plateNumberString;
+      if (plateNumber < 100) {
+        if (plateNumber < 10) {
+          plateNumberString = "00" + plateNumber.toString();
+        }
+        plateNumberString = "0" + plateNumber.toString();
+      }
+      plateNumberString = plateNumber.toString();
+      return randomPlateLetters + plateNumberString;
+    };
+    const records = [];
+    for (let id = 0; id <= 19; id++) {
+      const obj = {
+        id,
+        plates: randomPlate(),
+        firstName: names[id].name.first,
+        lastName: names[id].name.last,
+        phone: randomPhoneNumber(),
+        speed: generateRandomSpeed(60, 200),
+        latlong: "123456",
+      };
+      records.push(obj);
+      setUser(records);
+    }
+  }, [names, alphabet]);
+
   return (
     <>
-      {obj.map((el, i) => {
-        return <div key={i}>{el.plates}</div>;
+      {user.map((el, i) => {
+        return (
+          <div key={i}>
+            <div>
+              {el.firstName} {el.lastName}
+            </div>
+            <div>{el.plates}</div>
+            <div>{`+48 ${el.phone}`}</div>
+            <div>{el.speed}</div>
+          </div>
+        );
       })}
     </>
   );
